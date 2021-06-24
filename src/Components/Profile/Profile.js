f2e1bfe82cbb013b90aa3851846907fc747b650b
protyasha201 <protyasha0999@gmail.com>
2021-06-24 22:24:01 +0600

    updated profile section

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -4,16 +4,48 @@ import { UserContext } from "../../App";
 import Header from "../Header/Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faUserAlt } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Profile = () => {
   const [user, setUser] = useContext(UserContext);
+  const [showUserNameField, setShowUserNameField] = useState(false);
+  const [showEmailField, setShowEmailField] = useState(false);
+  const [showMobileNumberField, setShowMobileNumberField] = useState(false);
+  const [showPasswordField, setShowPasswordField] = useState(false);
+  const [showCountryField, setShowCountryField] = useState(false);
+  const [showDistrictField, setShowDistrictField] = useState(false);
+  const [showHouseField, setShowHouseField] = useState(false);
+
+  const showEditingSpace = (e) => {
+    if (e === "userName") {
+      setShowUserNameField(!showUserNameField);
+    }
+    if (e === "email") {
+      setShowEmailField(!showEmailField);
+    }
+    if (e === "mobileNumber") {
+      setShowMobileNumberField(!showMobileNumberField);
+    }
+    if (e === "password") {
+      setShowPasswordField(!showPasswordField);
+    }
+    if (e === "country") {
+      setShowCountryField(!showCountryField);
+    }
+    if (e === "district") {
+      setShowDistrictField(!showDistrictField);
+    }
+    if (e === "house") {
+      setShowHouseField(!showHouseField);
+    }
+  };
 
   const uploadProfileImage = () => {};
   return (
     <section>
       <Header />
       <div className="p-10 flex justify-center items-center flex-col">
-        <div className="shadow w-full flex flex-col items-center justify-center border p-5">
+        <div className="shadow w-full flex flex-col items-center justify-center border p-5 rounded">
           <div className="flex">
             <div className="h-20 w-20 flex justify-center items-center shadow border rounded-full">
               <FontAwesomeIcon
@@ -39,15 +71,18 @@ const Profile = () => {
                 {user.userName}
               </p>
               <FontAwesomeIcon
+                onClick={() => showEditingSpace("userName")}
                 className="cursor-pointer text-blue-300"
                 icon={faEdit}
               />
             </div>
-            <input
-              className="w-full hidden shadow p-1 border rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-              defaultValue={user.userName}
-              type="text"
-            />
+            {showUserNameField && (
+              <input
+                className="w-full shadow p-1 border rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                defaultValue={user.userName}
+                type="text"
+              />
+            )}
           </div>
         </div>
 
@@ -63,16 +98,19 @@ const Profile = () => {
                 Email: <span className="text-gray-400">{user.email}</span>
               </p>
               <FontAwesomeIcon
+                onClick={() => showEditingSpace("email")}
                 className="cursor-pointer text-blue-300"
                 icon={faEdit}
               />
             </div>
-            <input
-              name="email"
-              className="w-full hidden shadow p-1 border rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-              defaultValue={user.email}
-              type="email"
-            />
+            {showEmailField && (
+              <input
+                name="email"
+                className="w-full shadow p-1 border rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                defaultValue={user.email}
+                type="email"
+              />
+            )}
           </div>
 
           <div className="mt-2">
@@ -84,16 +122,19 @@ const Profile = () => {
                 </span>
               </p>
               <FontAwesomeIcon
+                onClick={() => showEditingSpace("mobileNumber")}
                 className="cursor-pointer text-blue-300"
                 icon={faEdit}
               />
             </div>
-            <input
-              name="mobileNumber"
-              className="w-full hidden shadow p-1 border rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-              defaultValue={user.mobileNumber}
-              type="text"
-            />
+            {showMobileNumberField && (
+              <input
+                name="mobileNumber"
+                className="w-full shadow p-1 border rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                defaultValue={user.mobileNumber}
+                type="text"
+              />
+            )}
           </div>
 
           <div className="mt-2">
@@ -105,16 +146,97 @@ const Profile = () => {
                 </span>
               </p>
               <FontAwesomeIcon
+                onClick={() => showEditingSpace("password")}
                 className="cursor-pointer text-blue-300"
                 icon={faEdit}
               />
             </div>
-            <input
-              name="password"
-              className="w-full hidden shadow p-1 border rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-              defaultValue={user.password}
-              type="password"
-            />
+            {showPasswordField && (
+              <input
+                name="password"
+                className="w-full shadow p-1 border rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                defaultValue={user.password}
+                type="password"
+              />
+            )}
+          </div>
+
+          <div className="mt-3">
+            <h1 className="condensed text-gray-500 font-bold text-xl">
+              Set Address
+            </h1>
+            <hr></hr>
+            <div className="mt-2">
+              <div className="flex">
+                <p className="text-red-300 montserrat font-bold">
+                  Country:{" "}
+                  <span className="text-gray-400">
+                    {user.country || "Set Current Country"}
+                  </span>
+                </p>
+                <FontAwesomeIcon
+                  onClick={() => showEditingSpace("country")}
+                  className="cursor-pointer text-blue-300"
+                  icon={faEdit}
+                />
+              </div>
+              {showCountryField && (
+                <input
+                  name="country"
+                  className="w-full shadow p-1 border rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                  defaultValue={user.country}
+                  type="text"
+                />
+              )}
+            </div>
+
+            <div className="mt-2">
+              <div className="flex">
+                <p className="text-red-300 montserrat font-bold">
+                  District:{" "}
+                  <span className="text-gray-400">
+                    {user.district || "Set Current District"}
+                  </span>
+                </p>
+                <FontAwesomeIcon
+                  onClick={() => showEditingSpace("district")}
+                  className="cursor-pointer text-blue-300"
+                  icon={faEdit}
+                />
+              </div>
+              {showDistrictField && (
+                <input
+                  name="district"
+                  className="w-full shadow p-1 border rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                  defaultValue={user.district}
+                  type="text"
+                />
+              )}
+            </div>
+
+            <div className="mt-2">
+              <div className="flex">
+                <p className="text-red-300 montserrat font-bold">
+                  House:{" "}
+                  <span className="text-gray-400">
+                    {user.house || "Set Current House"}
+                  </span>
+                </p>
+                <FontAwesomeIcon
+                  onClick={() => showEditingSpace("house")}
+                  className="cursor-pointer text-blue-300"
+                  icon={faEdit}
+                />
+              </div>
+              {showHouseField && (
+                <input
+                  name="house"
+                  className="w-full shadow p-1 border rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                  defaultValue={user.house}
+                  type="text"
+                />
+              )}
+            </div>
           </div>
         </div>
       </div>
